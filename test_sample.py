d9a0fb2d1bf0c9eb3e1bc45e444ea782dec4f8f9
AVM <miranska@users.noreply.github.com>
2022-02-09 13:31:42 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -17,7 +17,7 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_two(-2), 0)
         self.assertEqual(increment_by_two(0), 2)
         self.assertEqual(increment_by_two(3), 5)
-        
+
     def test_increment_by_three(self):
         """
         Test increments
