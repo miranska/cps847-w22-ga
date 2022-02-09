a3e0953c8a4dddb714d1888888b753dd33ca2e81
AVM <miranska@users.noreply.github.com>
2022-02-09 13:26:03 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,3 +10,12 @@ def increment_by_two(var_to_increment):
     :return: incremented variable
     """
     return var_to_increment + 2
+
+
+def increment_by_three(var_to_increment):
+    """
+    This function increments input by three
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 3
