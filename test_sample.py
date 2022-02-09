80b988606a8e7edaa979f6e82d5cc343ab04a215
AVM <miranska@users.noreply.github.com>
2022-02-09 13:30:11 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,7 @@ Test helper functions
 """
 
 import unittest
-from sample import increment_by_two
+from sample import increment_by_two, increment_by_three
 
 class TestSampleMethods(unittest.TestCase):
     """
@@ -17,6 +17,15 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_two(-2), 0)
         self.assertEqual(increment_by_two(0), 2)
         self.assertEqual(increment_by_two(3), 5)
+        
+    def test_increment_by_three(self):
+        """
+        Test increments
+        """
+        self.assertEqual(increment_by_three(-2), 1)
+        self.assertEqual(increment_by_three(0), 3)
+        self.assertEqual(increment_by_three(3), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
